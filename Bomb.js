d024b3d41884609269e203d8d8a2d7c2277becf6
Saito <kazutonight@gmail.com>
2020-06-29 23:34:54 +0200

    -

--- Bomb.js
+++ Bomb.js
@@ -1,59 +1,48 @@
 export default class Bomb {
+  constructor(x, y) {
+    this.posX = x;
+    this.posY = y;
+    this.newBomb = document.createElement("div");
+    this.gmZone = document.getElementById("game-zone");
+    this.newBomb.className = "bomb";
+    this.newBomb.style.top = this.posY;
+    this.newBomb.style.left = this.posX;
+    this.gmZone.appendChild(this.newBomb);
 
-    constructor(x, y) {
-        this.posX = x;
-        this.posY = y
-        this.newBomb = document.createElement('div');
-        this.gmZone = document.getElementById('game-zone')
-        this.newBomb.className = "bomb";
-        this.newBomb.style.top = this.posY;
-        this.newBomb.style.left = this.posX;
-        this.gmZone.appendChild(this.newBomb)
+    //this.newBomb.remove()
 
-        //this.newBomb.remove()
+    //this.timeExplo();
+  }
+  boom = () => {
+    console.log("boom");
+    //let bombCube = document.createElement('div');
+    //let gmZone = document.getElementById('game-zone')
+    bombCube.className = "bomb";
+    bombCube.style.top = y;
+    bombCube.style.left = x;
+    gmZone.appendChild(bombCube);
 
+    console.log(x + "bomb ");
+  };
 
-        //this.timeExplo();
+  timeExplo() {
+    setTimeout(() => {
+      this.animExplo();
+    }, 1000);
+  }
 
+  animExplo() {
+    this.newBomb.className = "activate";
+    this.newBomb.style.transform = "translate(-50%, -50%)";
+    setTimeout(() => {
+      this.deleteBomb();
+      console.log(this);
+    }, 600);
+  }
 
-    }
-    boom = () => {
-        console.log('boom')
-        //let bombCube = document.createElement('div');
-        //let gmZone = document.getElementById('game-zone')
-        bombCube.className = 'bomb';
-        bombCube.style.top = y;
-        bombCube.style.left = x;
-        gmZone.appendChild(bombCube);
-
-        console.log(x + 'bomb ');
-
-    }
-
-    timeExplo() {
-
-        setTimeout(() => {
-            this.animExplo()
-            setTimeout(() => {
-                this.deleteBomb()
-                console.log(this)
-            }, 600)
-
-        }, 1000)
-    }
-
-
-    animExplo() {
-        this.newBomb.className = 'activate'
-        this.newBomb.style.top -= 50 + 'px';
-        this.newBomb.style.left -= '150px';
-
-    }
-
-    deleteBomb() {
-        this.newBomb.remove();
-    }
-
+  deleteBomb() {
+    this.newBomb.remove();
+  }
 }
 /*
 Collision
