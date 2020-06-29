3a526628069788927136dcfa3fcaa4fb085e4634
Saito <kazutonight@gmail.com>
2020-06-29 20:27:28 +0200

    -

--- Bomb.js
+++ Bomb.js
@@ -33,14 +33,61 @@ export default class Bomb {
     timeExplo() {
 
         setTimeout(() => {
-            this.deleteBomb()
-            console.log(this)
+            this.animExplo()
+            setTimeout(() => {
+                this.deleteBomb()
+                console.log(this)
+            }, 600)
+
         }, 1000)
     }
 
+
+    animExplo() {
+        this.newBomb.className = 'activate'
+        this.newBomb.style.top -= 50 + 'px';
+        this.newBomb.style.left -= '150px';
+
+    }
 
     deleteBomb() {
         this.newBomb.remove();
     }
 
 }
+/*
+Collision
+function testerCollision() {
+    if (!(spriteBX > spriteAX + largeurSpriteA ||
+            spriteBX < spriteAX - largeurSpriteB ||
+            spriteBY > spriteAY + hauteurSpriteA ||
+            spriteBY < spriteAY - hauteurSpriteB)) {
+        // Collision
+    }
+}*/
+
+/* anim for bomb
+body{
+  width:200px;
+  height: 200px;
+  border: 1px solid black;
+}
+
+
+div
+{
+  position: absolute;
+  top:50px;
+  left:50px;
+  
+  width: 100px;
+  height: 100px;
+  border: 1px solid black;
+  border-radius: 50px;
+  animation-name: example;
+  animation-duration: 0.5s;
+  
+}
+
+
+}*/
